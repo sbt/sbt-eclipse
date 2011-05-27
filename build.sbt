a831f7dfe671d9e018016be7bdfd14e9e1f0b03e
Heiko Seeberger <heiko.seeberger@googlemail.com>
2011-05-27 10:19:34 +0200

    Added POM postprocessing in order to avoid issues with different SBT versions. Some cosmetic style changes.

--- build.sbt
+++ build.sbt
@@ -12,3 +12,15 @@ libraryDependencies += "org.scalaz" %% "scalaz-core" % "6.0.RC2"
 publishTo := Some("Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/")
 
 credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
+
+pomPostProcess := { (pom: scala.xml.Node) =>
+  import scala.xml._
+  import scala.xml.transform._
+  val rewriteRule = new RewriteRule {
+    override def transform(n: Node) = n match {
+      case elem @ Elem(_, "dependency", _, _, _*) if ((elem \ "groupId").text == "org.scala-tools.sbt") && ((elem \ "artifactId").text startsWith "sbt_")  => NodeSeq.Empty
+      case other => other
+    }
+  }
+  new RuleTransformer(rewriteRule)(pom)
+}
--- src/main/scala/SbtEclipsePlugin.scala
+++ src/main/scala/SbtEclipsePlugin.scala
@@ -24,149 +24,143 @@ import scalaz.Scalaz._
 
 object SbtEclipsePlugin extends Plugin {
 
-  override lazy val settings =
-    Seq(Keys.commands += eclipseCommand)
+  override lazy val settings = Seq(Keys.commands += eclipseCommand)
 
-  private lazy val eclipseCommand = {
+  private lazy val eclipseCommand = Command.command("eclipse") { state =>
 
-    Command.command("eclipse") { state =>
-
-      def setting[A](
-          key: SettingKey[A], 
-          errorMessage: => String,
-          configuration: Configuration = Configurations.Compile): Validation[NonEmptyList[String], A] = {
-        val extracted = Project.extract(state)
-        key in (extracted.currentRef, configuration) get extracted.structure.data match {
-          case Some(a) => a.success
-          case None => errorMessage.failNel
-        }
+    def setting[A](
+        key: SettingKey[A], 
+        errorMessage: => String,
+        configuration: Configuration = Configurations.Compile): Validation[NonEmptyList[String], A] = {
+      val extracted = Project.extract(state)
+      key in (extracted.currentRef, configuration) get extracted.structure.data match {
+        case Some(a) => a.success
+        case None => errorMessage.failNel
       }
+    }
 
-      def saveEclipseFiles(
-          projectName: String,
-          scalaVersion: String,
-          baseDirectory: File,
-          compileDirectories: Directories,
-          testDirectories: Directories,
-          libraries: Seq[File]) {
+    def saveEclipseFiles(
+        projectName: String,
+        scalaVersion: String,
+        baseDirectory: File,
+        compileDirectories: Directories,
+        testDirectories: Directories,
+        libraries: Seq[File]) {
 
-            def projectXml(name: String) =
-              <projectDescription>
-                <name>{ name }</name>
-                <buildSpec>
-                  <buildCommand>
-                    <name>org.scala-ide.sdt.core.scalabuilder</name>
-                  </buildCommand>
-                </buildSpec>
-                <natures>
-                  <nature>org.scala-ide.sdt.core.scalanature</nature>
-                  <nature>org.eclipse.jdt.core.javanature</nature>
-                </natures>
-              </projectDescription>
+      def projectXml(name: String) =
+        <projectDescription>
+          <name>{ name }</name>
+          <buildSpec>
+            <buildCommand>
+              <name>org.scala-ide.sdt.core.scalabuilder</name>
+            </buildCommand>
+          </buildSpec>
+          <natures>
+            <nature>org.scala-ide.sdt.core.scalanature</nature>
+            <nature>org.eclipse.jdt.core.javanature</nature>
+          </natures>
+        </projectDescription>
 
-            def classpathXml(
-                compileSourceDirectories: Seq[File],
-                compileResourceDirectories: Seq[File],
-                classDirectory: File,
-                testSourceDirectories: Seq[File],
-                testResourceDirectories: Seq[File],
-                testClassDirectory: File,
-                baseDirectory: File) = {
+      def classpathXml(
+          compileSourceDirectories: Seq[File],
+          compileResourceDirectories: Seq[File],
+          classDirectory: File,
+          testSourceDirectories: Seq[File],
+          testResourceDirectories: Seq[File],
+          testClassDirectory: File,
+          baseDirectory: File) = {
 
-              def srcEntries(directories: Seq[File], output: File): NodeSeq =
-                directories flatMap { directory =>
-                  if (directory.exists) {
-                    logger(state).debug("""Creating src entry for directory "%s".""".format(directory))
-                    val relative = IO.relativize(baseDirectory, directory).get
-                    val relativeOutput = IO.relativize(baseDirectory, output).get
-                    <classpathentry kind="src" path={ relative.toString } output={ relativeOutput.toString } />
-                  } else {
-                    logger(state).debug("""Skipping src entry for non-existent directory "%s".""".format(directory))
-                    NodeSeq.Empty
-                  }
-                }
-
-             def libEntries(libs: Seq[File]): NodeSeq =
-               libs collect {
-                 case Path(path) if !(path endsWith "scala-library.jar") => path
-               } flatMap { path =>
-                 logger(state).debug("""Creating lib entry for dependency "%s".""".format(path))
-                 <classpathentry kind="lib" path={ path } />
-               }
-
-              <classpath>{
-                srcEntries(compileSourceDirectories, classDirectory) ++
-                srcEntries(compileResourceDirectories, classDirectory) ++
-                srcEntries(testSourceDirectories, testClassDirectory) ++
-                srcEntries(testResourceDirectories, testClassDirectory) ++
-                libEntries(libraries) ++
-                <classpathentry kind="con" path="org.scala-ide.sdt.launching.SCALA_CONTAINER"/>
-                <classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.6"/>
-                <classpathentry kind="output" path="target/classes"/>
-              }</classpath>
+        def srcEntries(directories: Seq[File], output: File) =
+          directories flatMap { directory =>
+            if (directory.exists) {
+              logger(state).debug("""Creating src entry for directory "%s".""".format(directory))
+              val relative = IO.relativize(baseDirectory, directory).get
+              val relativeOutput = IO.relativize(baseDirectory, output).get
+              <classpathentry kind="src" path={ relative.toString } output={ relativeOutput.toString }/>
+            } else {
+              logger(state).debug("""Skipping src entry for non-existent directory "%s".""".format(directory))
+              NodeSeq.Empty
             }
+          }
 
-        XML.save(".project", projectXml(projectName))
-        XML.save(
-            ".classpath", 
-            classpathXml(
-                compileDirectories.sources, 
-                compileDirectories.resources, 
-                compileDirectories.clazz, 
-                testDirectories.sources, 
-                testDirectories.resources, 
-                testDirectories.clazz, 
-                baseDirectory), 
-            "UTF-8", 
-            true)
+        def libEntries(libs: Seq[File]) =
+          libs collect {
+            case PathExtractor(path) if !(path endsWith "scala-library.jar") => path
+          } flatMap { path =>
+            logger(state).debug("""Creating lib entry for dependency "%s".""".format(path))
+            <classpathentry kind="lib" path={ path }/>
+          }
+
+        <classpath>{
+          srcEntries(compileSourceDirectories, classDirectory) ++
+          srcEntries(compileResourceDirectories, classDirectory) ++
+          srcEntries(testSourceDirectories, testClassDirectory) ++
+          srcEntries(testResourceDirectories, testClassDirectory) ++
+          libEntries(libraries) ++
+          <classpathentry kind="con" path="org.scala-ide.sdt.launching.SCALA_CONTAINER"/>
+          <classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.6"/>
+          <classpathentry kind="output" path="target/classes"/>
+        }</classpath>
       }
 
-      logger(state).debug("Trying to create an Eclipse project for you ...")
+      XML.save(".project", projectXml(projectName))
+      XML.save(".classpath", 
+          classpathXml(
+              compileDirectories.sources,
+              compileDirectories.resources,
+              compileDirectories.clazz,
+              testDirectories.sources,
+              testDirectories.resources,
+              testDirectories.clazz,
+              baseDirectory),
+          "UTF-8",
+          true)
+    }
 
-      val projectName = setting(Keys.name, "Missing project name!")
-      val scalaVersion =
-        setting(Keys.scalaVersion, "Missing Scala version!") match {
-          case f @ Failure(_) => f
-          case Success(s) if (s startsWith "2.9") => s.success
-          case _ => "Only for Scala 2.9!".failNel
-        }
-      val baseDirectory = setting(Keys.baseDirectory, "Missing base directory!")
-      val compileDirectories =
-        (setting(Keys.unmanagedSourceDirectories, "Missing unmanaged source directories!") |@|
-            setting(Keys.unmanagedResourceDirectories, "Missing unmanaged resource directories!") |@|
-            setting(Keys.classDirectory, "Missing class directory!")) {
-          Directories
-        }
-      val testDirectories =
-        (setting(Keys.unmanagedSourceDirectories, "Missing unmanaged test source directories!", Configurations.Test) |@|
-            setting(Keys.unmanagedResourceDirectories, "Missing unmanaged test resource directories!", Configurations.Test) |@|
-            setting(Keys.classDirectory, "Missing test class directory!", Configurations.Test)) {
-          Directories
-        }
-      val libraries = // Configurations.Test contains items from Configurations.Compile!
-        Project.evaluateTask(Keys.externalDependencyClasspath in Configurations.Test, state) match {
-          case Some(Value(attributedLibs)) => (attributedLibs map { _.data }).success
-          case Some(Inc(_)) => "Error determining compile libraries: %s".failNel
-          case None => "Missing compile libraries!".failNel
-        }
+    logger(state).debug("Trying to create an Eclipse project for you ...")
 
-      (projectName |@| scalaVersion |@| baseDirectory |@| compileDirectories |@| testDirectories |@| libraries) {
-        saveEclipseFiles
-      } match {
-        case Success(_) =>
-          logger(state).info("Successfully created an Eclipse project for you. Have fun!")
-          state
-        case Failure(errors) =>
-          logger(state).error(errors.list mkString ", ")
-          state.fail
+    val projectName = setting(Keys.name, "Missing project name!")
+    val scalaVersion =
+      setting(Keys.scalaVersion, "Missing Scala version!") match {
+        case f @ Failure(_) => f
+        case Success(s) if (s startsWith "2.9") => s.success
+        case _ => "Only for Scala 2.9!".failNel
       }
+    val baseDirectory = setting(Keys.baseDirectory, "Missing base directory!")
+    val compileDirectories =
+      (setting(Keys.unmanagedSourceDirectories, "Missing unmanaged source directories!") |@|
+          setting(Keys.unmanagedResourceDirectories, "Missing unmanaged resource directories!") |@|
+          setting(Keys.classDirectory, "Missing class directory!")) {
+        Directories
+      }
+    val testDirectories =
+      (setting(Keys.unmanagedSourceDirectories, "Missing unmanaged test source directories!", Configurations.Test) |@|
+          setting(Keys.unmanagedResourceDirectories, "Missing unmanaged test resource directories!", Configurations.Test) |@|
+          setting(Keys.classDirectory, "Missing test class directory!", Configurations.Test)) {
+        Directories
+      }
+    val libraries = // Configurations.Test contains items from Configurations.Compile!
+      Project.evaluateTask(Keys.externalDependencyClasspath in Configurations.Test, state) match {
+        case Some(Value(attributedLibs)) => (attributedLibs map { _.data }).success
+        case Some(Inc(_)) => "Error determining compile libraries: %s".failNel
+        case None => "Missing compile libraries!".failNel
+      }
+
+    (projectName |@| scalaVersion |@| baseDirectory |@| compileDirectories |@| testDirectories |@| libraries) {
+      saveEclipseFiles
+    } match {
+      case Success(_) =>
+        logger(state).info("Successfully created an Eclipse project for you. Have fun!")
+        state
+      case Failure(errors) =>
+        logger(state).error(errors.list mkString ", ")
+        state.fail
     }
   }
 
   private case class Directories(sources: Seq[File], resources: Seq[File], clazz: File)
 
-  private object Path {
-    def unapply(file: File): Option[String] =
-      Some(file.getAbsolutePath)
+  private object PathExtractor {
+    def unapply(file: File): Option[String] = Some(file.getAbsolutePath)
   }
 }
