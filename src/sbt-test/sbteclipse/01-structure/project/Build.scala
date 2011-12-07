79d3345c20eb72c2b2c8ac7122d9da311d32c794
Heiko Seeberger <heiko.seeberger@googlemail.com>
2011-12-07 18:36:28 +0000

    Added tests for structure.

---
+++ src/sbt-test/sbteclipse/01-structure/project/Build.scala
@@ -0,0 +1,40 @@
+import sbt._
+import sbt.Keys._
+
+object Build extends Build {
+
+  lazy val root = Project(
+    "root",
+    new File("."),
+    aggregate = Seq(sub)
+  )
+
+  lazy val sub = Project(
+    "sub",
+    new File("sub"),
+    aggregate = Seq(suba, subb)
+  )
+
+  lazy val suba = Project(
+    "suba",
+    new File("sub/suba"),
+    settings = Seq(
+      libraryDependencies ++= Seq(
+        "com.weiglewilczek.slf4s" %% "slf4s" % "1.0.7"
+      )
+    )
+  )
+
+  lazy val subb = Project(
+    "subb",
+    new File("sub/subb"),
+    settings = Seq(
+      libraryDependencies ++= Seq(
+        "com.weiglewilczek.slf4s" %% "slf4s" % "1.0.7" % "test",
+        "biz.aQute" % "bndlib" % "1.50.0"
+      ),
+      scalacOptions := Seq("-unchecked", "-deprecation")
+    ),
+    dependencies = Seq(suba, suba % "test->test")
+  )  
+}
