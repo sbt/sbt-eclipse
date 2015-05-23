3f0101ff1b9cb5610f1d941d0a1926f5febca4df
Mirco Dotta <mirco.dotta@gmail.com>
2015-05-23 12:45:19 +0200

    increasing permgen to 3072M

--- src/sbt-test/sbteclipse/06-multi-scala-version-support/project/Build.scala
+++ src/sbt-test/sbteclipse/06-multi-scala-version-support/project/Build.scala
@@ -13,7 +13,7 @@ object Build extends Build {
     "suba",
     new File("suba"),
     settings = Project.defaultSettings ++ Seq(
-      scalaVersion := "2.10.4"
+      scalaVersion := "2.10.3"
     )
   )
   
@@ -21,7 +21,7 @@ object Build extends Build {
     "subb",
     new File("subb"),
     settings = Project.defaultSettings ++ Seq(
-      scalaVersion := "2.11.4"
+      scalaVersion := "2.11.3"
     )
   )
   
