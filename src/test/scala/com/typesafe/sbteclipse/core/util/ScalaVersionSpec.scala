ab608cc3347a8cabc1b04f9e8553ebf806171c16
Koen Daenen <daenenk@users.noreply.github.com>
2018-01-15 17:41:33 +0100

    Improved support for multiple scala versions (#356)
    Improved support for multiple scala versions

--- src/test/scala/com/typesafe/sbteclipse/core/util/ScalaVersionSpec.scala
+++ src/test/scala/com/typesafe/sbteclipse/core/util/ScalaVersionSpec.scala
@@ -7,27 +7,27 @@ import ScalaVersion.FullScalaVersion
 class ScalaVersionSpec extends WordSpec with Matchers {
   "ScalaVersion" should {
     """parse Scala version "2.12.0"""" in {
-      ScalaVersion.parse("2.12.0") shouldEqual FullScalaVersion(2, 12, 0, None)
+      ScalaVersion.parse("2.12", "2.12.0") shouldEqual FullScalaVersion(2, 12, 2, 12, 0, None)
     }
 
     """parse Scala version "2.12.0-SNAPSHOT"""" in {
-      ScalaVersion.parse("2.12.0-SNAPSHOT") shouldEqual FullScalaVersion(2, 12, 0, Some("SNAPSHOT"))
+      ScalaVersion.parse("2.12", "2.12.0-SNAPSHOT") shouldEqual FullScalaVersion(2, 12, 2, 12, 0, Some("SNAPSHOT"))
     }
 
     """parse Scala version "2.12.0-RC10"""" in {
-      ScalaVersion.parse("2.12.0-RC10") shouldEqual FullScalaVersion(2, 12, 0, Some("RC10"))
+      ScalaVersion.parse("2.12", "2.12.0-RC10") shouldEqual FullScalaVersion(2, 12, 2, 12, 0, Some("RC10"))
     }
 
     """parse Scala version "2.12.0-M1"""" in {
-      ScalaVersion.parse("2.12.0-M1") shouldEqual FullScalaVersion(2, 12, 0, Some("M1"))
+      ScalaVersion.parse("2.12", "2.12.0-M1") shouldEqual FullScalaVersion(2, 12, 2, 12, 0, Some("M1"))
     }
 
     """parse Scala version "2.12.0-51e77037f2adc4ffa7421aa36803a5874292b70d"""" in {
-      ScalaVersion.parse("2.12.0-51e77037f2adc4ffa7421aa36803a5874292b70d") shouldEqual FullScalaVersion(2, 12, 0, Some("51e77037f2adc4ffa7421aa36803a5874292b70d"))
+      ScalaVersion.parse("2.12", "2.12.0-51e77037f2adc4ffa7421aa36803a5874292b70d") shouldEqual FullScalaVersion(2, 12, 2, 12, 0, Some("51e77037f2adc4ffa7421aa36803a5874292b70d"))
     }
 
     """fail to parse "2.12"""" in {
-      ScalaVersion.parse("2.12") shouldEqual NoScalaVersion
+      ScalaVersion.parse("2.12", "2.12") shouldEqual NoScalaVersion
     }
   }
 }
